b72dbe80a1cb4c924d1f76acb8b374c7a6d805a7
Lennart Luttkus <lennart.luttkus@uni-a.de>
2024-01-08 15:57:42 +0100

    keep same view box

--- buildings_only_svg.py
+++ buildings_only_svg.py
@@ -1,12 +1,10 @@
 import xml.etree.ElementTree as ET
 
 # The color to filter by (already in percentage format)
-# rgb_color_percent = (85.098039, 81.568627, 78.823529)
-# rgb_color_str = 'rgb({:.6f}%, {:.6f}%, {:.6f}%)'.format(*rgb_color_percent)
 rgb_color_str = 'rgb(85.098039%,81.568627%,78.823529%)'
 
 # Parse the SVG file
-tree = ET.parse('svg_maps/map (2).svg')
+tree = ET.parse('svg_maps/map2.svg')
 root = tree.getroot()
 
 # Identify all elements with the specified color
@@ -14,6 +12,11 @@ elements = [elem for elem in root.iter() if 'style' in elem.attrib and rgb_color
 
 # Create a new SVG file with just the elements
 new_root = ET.Element('svg', xmlns="http://www.w3.org/2000/svg")
+
+# Copy viewBox attribute from the original root to the new root
+if 'viewBox' in root.attrib:
+    new_root.attrib['viewBox'] = root.attrib['viewBox']
+
 for elem in elements:
     new_root.append(elem)
 
